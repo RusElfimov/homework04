3bcdfdd317d1d1219afb7d2fda54c6f091d589c2
RusElfimov <ruselfimovbox@outlook.com>
2022-10-20 17:48:25 +0300

    work7

---
+++ work7/Program.cs
@@ -0,0 +1,26 @@
+﻿//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+int a = Convert.ToInt32(Console.ReadLine());
+double [] arr = new double[a];
+for( int i = 0; i < a; i++)
+{
+ arr[i] = new Random().NextDouble();
+ Console.Write(arr[i] + " ");
+}
+double max = arr[0];
+double min = arr[0];
+for( int i = 1; i < a; i++)
+{
+if (arr[i] > max)
+{
+    max = arr[i];
+}
+else if (arr[i] < min)
+{
+    min = arr[i];
+}
+}
+Console.WriteLine(" ");
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.Write("Разница между максимальным и минимальным значением = ");
+Console.Write(max - min);
