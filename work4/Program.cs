549876389fa59c0407839b56fb96e8149fdd9e6a
RusElfimov <ruselfimovbox@outlook.com>
2022-10-20 17:48:01 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,17 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int a = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[a];
+int n = 0;
+int m = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    arr[i] = new Random().Next(0, m);
+    Console.Write(arr[i] + " ");
+    if (arr[i] > 10 & arr[i] < 99)
+    {
+        n = n + 1;
+    }
+}
+Console.WriteLine(" ");
+Console.WriteLine(n);
