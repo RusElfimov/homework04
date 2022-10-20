3ee16cce3dc7c8e9ed8d9fa530857bf658d4cb08
RusElfimov <ruselfimovbox@outlook.com>
2022-10-20 17:48:09 +0300

    work5

---
+++ work5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+int a = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int[a];
+int sum = 0;
+int m = Convert.ToInt32(Console.ReadLine()); // Числовой диапазон
+for( int i = 0; i < a; i++)
+{
+ arr[i] = new Random().Next(0, m);
+ Console.Write(arr[i] + " ");
+ if (i % 2 != 0 )
+ {
+   sum = sum + arr[i]; 
+ }
+}
+Console.WriteLine(" ");
+Console.WriteLine(sum);
